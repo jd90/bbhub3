6c0e9d6b0274f8bafc6f998b495cd24771d67c0c
jd90 <1418146@rgu.ac.uk>
2016-03-07 12:21:25 +0000

    orite pal

--- OwnerSignIn.php
+++ OwnerSignIn.php
@@ -20,87 +20,53 @@ session_start();
 </head>
 
 <header>
-
-    <div class='container'>
     <section class="right">
         <div class="left">
-            <a href="Home.php"><img src = "assets/b&blogotransparent.png" id="img"></a>
+            <img src = "assets/b&blogotransparent.png" id="img">
 
 
         </div>
         <div class=rightside">
-
-
-    <?php
-    if ($_SESSION["user"] != null) {
-        echo "<p id='loginText'>Currently signed in as: " . $_SESSION["user"];
-        echo "    not you?</p><button id='logout()' onclick='logout()'>LOGOUT</button>";
-
-        //header("Location: OwnerReviewPage.php"); ||
-
-        showOwnerReviewPage();
-
-    }else{
-        echo "<p id='loginText'>currently not logged in";
-
-        showOwnerSignInPage();
-    }
-
-    ?>
-            </div>
-    <script>
-        function logout() {
-            window.location = "http://thebbhubdotcom.azurewebsites.net/home.php?value=logout";
-        }
-    </script>
-
-
-    <?
-
-    function showOwnerSignInPage()
-    {
-
-        $htmlOwnerSignInPage =
-<<<HTMLPAGE
-
+            <?php
+            if ($_SESSION["user"] != null) {
+                echo "<p id='loginText'>Currently signed in as: " . $_SESSION["user"];
+                echo "    not you?</p><button id='logout()' onclick='logout()'>LOGOUT</button>";
+                //header("Location: OwnerReviewPage.php"); ||
+                showOwnerReviewPage();
+            }else{
+                echo "<p id='loginText'>currently not logged in";
+                showOwnerSignInPage();
+            }
+            ?>
+            <script>
+                function logout() {
+                    window.location = "http://iainscott-rgu.azurewebsites.net/home.php?value=logout";
+                }
+            </script>
+            <?
+            function showOwnerSignInPage()
+            {
+                $htmlOwnerSignInPage =
+                    <<<HTMLPAGE
+                            </div>
         </section>
-
-</div>
         </header>
-
-
-<div class='nav'><nav>
-
-
-
+<div class='nav'>
+      <nav>
         <ul class="moveright">
-
-
-            <li><a href="B&Bregistration.php">Register</a></li>
-            <!-- <li><a href="B&Bregistration.php">Register</a></li> -->
+            <li><a href="B&Bregistration.html">Register</a></li>
+            <!-- <li><a href="B&Bregistration.html">Register</a></li> -->
             <li><a href="OwnerSignIn.php">Owner's Page</a></li>
             <li><a href="Home.php">Search</a></li>
-
-
         </ul>
-
     </nav>
-
-
-
-
+</div>
 <hr width="100%" align="left" size="1" color="#d3d3d3">
 <body>
-
-
 <main>
-
-
    <table class='table1'>
             <tr><td colspan='2'>Owner's Page</td></tr>
-
             <tr><td class='small'>* Required Fields</td></tr>
-
             <tr><td>
                 <label for='email'>Email: *</label></td>
                 <td><input type='text' class='inputform' id='email' name='email' placeholder='email' size='30' maxlength='50' required /></td>
@@ -110,25 +76,14 @@ session_start();
                 <td>    <input type='password' class='inputform' id='password' name='password' placeholder='enter password' required>
                 </td>
             </tr>
-
     <tr><td></td><td><input id='submit' type='button' value='Sign In'></td></tr>
     <form class='table1' id='myForm' name = 'myForm' action='OwnerRegistration.php' >
     <tr><td></td><td><input id='submit' type = 'submit' value = 'Register'/></td></tr>
-
-
-
-
-
     <!-- The result of the search will be rendered inside this div -->
     <tr><td colspan='2'><p id='result' style='color: #ff0006'></p></td></tr>
 </table>
             </form>
-
-
-
     <div class='right2'>
-
-
             <table class='table2'>
                 <tr><td colspan='2'><p>Message Board - advertisements - user info</p></td></tr>
                 <tr class='tr2'><td class='t2'><p>row 1</p></td></tr>
@@ -140,99 +95,46 @@ session_start();
                 <tr class='tr2'><td class='t2'><p>row 7</p></td></tr>
                 <tr class='tr2'><td class='t2'><p>row 8</p></td></tr>
                 <tr class='tr2'><td class='t2'><p>row 9</p></td></tr>
-
-
-
-
-
-
-
             </table>
     </div>
-
-
-
+    <hr width='100%' align='left' size='1' color=''#2f4f4f'>
 </main>
-
-</div>
-</body><div class='foot'>
+<div class='foot'>
 <footer>
-
     <p>Copyright. Team D Solutions.</p>
-</footer>
-</div>
+</footer></div>
+</body>
 </html>
-
-
-
-
-
 HTMLPAGE;
-
-        print($htmlOwnerSignInPage);
-    }
-
-    function showOwnerReviewPage()
-    {
-
-
-        $htmlOwnerReviewPage =
-<<<HTMLPAGE2
-
-            </section>
-
-                               </header>
-
-
+                print($htmlOwnerSignInPage);
+            }
+            function showOwnerReviewPage()
+            {
+                $htmlOwnerReviewPage =
+                    <<<HTMLPAGE2
+                                                    </header>
 <div class='nav'>
-  <nav>
-
-
-
+    <nav>
         <ul class="moveright">
-
-            <!--<li><a href="B&Bregistration.php">Help</a></li>-->
+            <!--<li><a href="B&Bregistration.html">Help</a></li>-->
             <li><a href="Customerinfo.html">Register</a></li>
-            <!-- <li><a href="B&Bregistration.php">Register</a></li> -->
+            <!-- <li><a href="B&Bregistration.html">Register</a></li> -->
             <li><a href="OwnerSignIn.php">Owner's Page</a></li>
             <li><a href="Home.php">Search</a></li>
-
-
         </ul>
-
     </nav>
 </div>
-
-
-
 <hr width="100%" align="left" size="1" color="#d3d3d3">
 <body>
-
-
 <main>
-
-
    <table class='table1'>
             <tr><td colspan='2'>Owner's Page</td></tr>
-
-
-
     <tr><td>You are on the owner's review page!</td></tr>
-
-
-
-
-
     <!-- The result of the search will be rendered inside this div -->
     <tr><td colspan='2'><p id='result' style='color: #ff0006'></p></td></tr>
 </table>
             </form>
-
-
-
     <div class='right2'>
-
-
             <table class='table2'>
                 <tr><td colspan='2'><p>Message Board - advertisements - user info</p></td></tr>
                 <tr class='tr2'><td class='t2'><p>row 1</p></td></tr>
@@ -244,42 +146,18 @@ HTMLPAGE;
                 <tr class='tr2'><td class='t2'><p>row 7</p></td></tr>
                 <tr class='tr2'><td class='t2'><p>row 8</p></td></tr>
                 <tr class='tr2'><td class='t2'><p>row 9</p></td></tr>
-
-
-
-
-
-
-
             </table>
     </div>
-
-
-
+    <hr width='100%' align='left' size='1' color=''#2f4f4f'>
 </main>
-
-
-</div>
-</body><div class='foot'>
+<div class='foot'>
 <footer>
-
     <p>Copyright. Team D Solutions.</p>
-</footer>
-</div>
+</footer></div>
+</body>
 </html>
-
-
-
+        ";
 HTMLPAGE2;
-
-        print($htmlOwnerReviewPage);
-
-    }
-
-
-
-
-    ?>
-
-
-
+                print($htmlOwnerReviewPage);
+            }
+            ?>
