5a72f105deefade9343aa772f6e555123405b206
jd90 <1418146@rgu.ac.uk>
2016-03-07 15:10:31 +0000

    orite pal

---
+++ bbReviewPage.php
@@ -0,0 +1,114 @@
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Results</title>
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+
+    <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,800' rel='stylesheet' type='text/css'>
+
+
+</head>
+
+<div class='container'>
+    <header>
+        <section class="right">
+            <div class="left">
+                <a href="Home.php"><img src ="assets/b&blogotransparent.png" id="img"></a>
+
+
+            </div>
+            <div class="rightside">
+                <?php
+                if ($_SESSION["user"] != null) {
+                    echo "<p id='loginText'>Currently signed in as: " . $_SESSION["user"];
+                    echo "    not you?</p><button id='logout()' onclick='logout()'>LOGOUT</button>";
+                }else{
+                    echo "<p id='loginText'>currently not logged in";
+                }
+
+                ?>
+            </div>
+        </section>
+
+
+
+    </header>
+
+    <div class="nav">
+        <nav>
+            <ul class="moveright">
+
+                <!--<li><a href="B&Bregistration.html">Help</a></li>-->
+                <li><a href="OwnerRegistration.php">Register</a></li>
+                <!-- <li><a href="B&Bregistration.html">Register</a></li> -->
+                <li><a href="OwnerSignIn.php">Owner's Page</a></li>
+                <li><a href="Home.php">Search</a></li>
+
+
+            </ul>
+
+        </nav>
+    </div>
+
+
+
+
+
+    <body>
+    <main>
+
+
+        <div class="main">
+            <h3 class="pagetitle" >Here are your search results...</h3>
+
+            <?php
+            $email = $_SESSION['user'];
+            $conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
+            $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            try{
+                $st = $conn-> query("SELECT * FROM [B&B] WHERE [email] = '$email'");
+
+                echo "<p>These B&Bs are registered as belonging to: <strong>{$email}</strong>: </p>";
+
+                foreach($st->fetchAll() as $row) {
+                    $newhtml =
+                        <<<NEWHTML
+                            <div class="table1">
+    <p><strong>{$row[city]}</strong></p>
+    <p><strong>{$row[bbname]}</strong></p>
+    <p><strong>{$row[address]}</strong></p>
+    <p><strong>{$row[email]}</strong></p>
+
+
+
+
+    <p><a href="Customerinfo.html"><input type="submit" value="BOOK" /></a></p>
+
+
+
+</div>
+NEWHTML;
+                    print($newhtml);
+                }
+            }
+            catch(PDOException $e)
+            {print"$e";}
+            ?>
+        </div>
+    </main>
+
+</div>
+</body><div class="foot">
+    <footer>
+
+        <p>Copyright. Team D Solutions.</p>
+    </footer></div>
+</html>
